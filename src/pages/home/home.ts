c63aaebfd0690c504bfe1070f174e55dc7fcf77a
rina0620 <rina@l-adder.com>
2018-04-17 20:14:41 +0900

    0417_

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,40 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { HttpClient } from '@angular/common/http';
+import { IonicPage, NavController, LoadingController, Platform } from 'ionic-angular';
 
+@IonicPage()
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  posts: {
+    ID: number,
+    title: string,
+    content: string,
+    date: string,
+  }[] = [];
 
-  constructor(public navCtrl: NavController) {
+  constructor(
+    public navCtrl: NavController,
+    public http: HttpClient,
+    public loadingCtrl: LoadingController,
+    public platform: Platform
+  ) {}
 
+ionViewDidLoad(){
+  if(!this.platform.is('android')){
+    ga('send', 'pageview', '/signin');
   }
+  let loading = this.loadingCtrl.create();
+  loading.present();
+
+  this.http
+  .get('https://public-api.wordpress.com/rest/v1.1/sites/ionicjp.wordpress.com/posts')
+  .subscribe(data => {
+    this.posts = data['posts'];
+    loading.dismiss();
+  });
+ }
 
 }
